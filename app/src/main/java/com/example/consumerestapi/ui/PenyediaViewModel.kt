a210caccadd3824c971234bb8cd6929041586752
brilly123 <b.amindia.ft21@mail.umy.ac.id>
2023-12-28 00:05:27 +0700

    Initial commit

---
+++ app/src/main/java/com/example/consumerestapi/ui/PenyediaViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.consumerestapi.ui
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.consumerestapi.KontakAplication
+import com.example.consumerestapi.ui.home.viewmodel.HomeViewModel
+import com.example.consumerestapi.ui.home.viewmodel.InsertViewModel
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiKontak().container.kontakRepository)
+        }
+
+        initializer {
+            InsertViewModel(aplikasiKontak().container.kontakRepository)
+        }
+
+    }
+}
+fun CreationExtras.aplikasiKontak(): KontakAplication =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KontakAplication)
