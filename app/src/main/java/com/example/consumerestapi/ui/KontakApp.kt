c3727818c0c22b084b8fedd3ec892387c02967e4
brilly123 <b.amindia.ft21@mail.umy.ac.id>
2023-12-28 00:07:08 +0700

    mengedit kontakapp

--- app/src/main/java/com/example/consumerestapi/ui/KontakApp.kt
+++ app/src/main/java/com/example/consumerestapi/ui/KontakApp.kt
@@ -2,8 +2,12 @@ package com.example.consumerestapi.ui
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -22,9 +26,8 @@ import com.example.consumerestapi.ui.home.viewmodel.HomeViewModel
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun KontakApp(
-    homeViewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory
-    )
-) {
+    homeViewModel: HomeViewModel = viewModel(factory =  PenyediaViewModel.Factory )
+){
     val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
     Scaffold (
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
@@ -37,23 +40,50 @@ fun KontakApp(
         ) {
             HomeScreen(
                 kontakUIState = homeViewModel.kontakUIState,
-                retryAction = homeViewModel::getKontak
+                retryAction = homeViewModel::getKontak,
             )
         }
+
     }
 }
 
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopAppBar(scrollBehavior: TopAppBarScrollBehavior, modifier: Modifier = Modifier) {
-    CenterAlignedTopAppBar(
-        scrollBehavior = scrollBehavior,
+fun TopAppBar(scrollBehavior: TopAppBarScrollBehavior, modifier: Modifier = Modifier){
+    CenterAlignedTopAppBar(scrollBehavior = scrollBehavior,
         title = {
             Text(
-                text = stringResource(id = R.string.app_name),
-                style = MaterialTheme.typography.headlineSmall,
+                text = stringResource(R.string.app_name),
+                style = MaterialTheme.typography.headlineSmall
             )
         },
         modifier = modifier
-       )
+    )
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBarKontak(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {}
+){
+    CenterAlignedTopAppBar(title = { Text(title)},
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack){
+                IconButton(onClick = navigateUp){
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = " "
+                    )
+                }
+            }
+        }
+    )
 }
